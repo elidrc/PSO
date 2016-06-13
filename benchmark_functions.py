9b3f01f668a1a3394473ecbbd06f91fc4ad36309
elidrc <elidrc@gmail.com>
2016-06-12 19:09:28 -0700

    Add files via upload

--- benchmark_functions.py
+++ benchmark_functions.py
@@ -1,24 +1,56 @@
-import math
+from math import *
+
 
 def sphere(solution):
-    d = len(solution)
+    dimensions = len(solution)
     sumatory = 0
-    for i in range(0, d):
+    for i in range(0, dimensions):
         sumatory += solution[i] ** 2
+
     return sumatory
 
-def rosenbrock():
-    pass
 
-def ackley(solution, a=20, b=0.2, c=2*math.pi):
-    d = len(solution)
-    sum1 = 0
-    sum2 = 0
+def rosenbrock(solution):
+    dimensions = len(solution)
+    sumatory = 0
 
-    for i in range(0, d):
-        sum1 += solution[i] ** 2
-        sum2 += math.cos(c * solution[i])
-    term1 = -a * math.exp(-b * math.sqrt(sum1 / d))
-    term2 = -math.exp(sum2 / d)
+    for i in range(0, dimensions):
+        first_term = (solution[i + 1] - solution[i] ** 2) ** 2
+        second_term = (solution[i] - 1) ** 2
+        sumatory += 100 * first_term + second_term
 
-    return term1 + term2 + a + math.exp(1)
+    return sumatory
+
+
+def ackley(solution, a=20, b=0.2, c=2*pi):
+    dimensions = len(solution)
+    first_sumatory = 0
+    second_sumatory = 0
+
+    for i in range(0, dimensions):
+        first_sumatory += solution[i] ** 2
+        second_sumatory += cos(c * solution[i])
+    first_term = -a * exp(-b * sqrt(first_sumatory / dimensions))
+    second_term = -exp(second_sumatory / dimensions)
+
+    return first_term + second_term + a + exp(1)
+
+def griewank(solution):
+    dimensions = len(solution)
+    sumatory = 0
+    product = 1
+
+    for i in range(0, dimensions):
+        sumatory += solution[i] ** 2 / 4000.0
+        product *= cos(solution[i] / sqrt(i))
+
+    return sumatory - product + 1
+
+def rastrigin(solution):
+    dimensions = len(solution)
+    sumatory = 0
+
+    for i in range(0, dimensions):
+        sumatory += (solution[i] ** 2 - 10 * cos(2 * pi * solution[i]))
+
+    return 10 * dimensions + sumatory
